3a460b0d6646dfb71a71adad8401d5334db09fd4
armando5569 <armsanchez5@gmail.com>
2019-06-21 16:16:32 -0500

    init commit

---
+++ src/main/java/Capstone/Project/CodecampProject/CodecampProjectApplication.java
@@ -0,0 +1,13 @@
+package Capstone.Project.CodecampProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecampProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecampProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Capstone/Project/CodecampProject/CodecampProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package Capstone.Project.CodecampProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodecampProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
